28b5c4b2de770a2f0719585646e43cc7cdea8c89
theNvN <thenvnsahu@gmail.com>
2023-01-22 03:25:38 +0530

    refactor: isolate types

--- apps/protocol/contracts/libraries/DataTypes.sol
+++ apps/protocol/contracts/libraries/DataTypes.sol
@@ -1,56 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-library DataTypes {
-  enum TxType {
-    DEPOSIT,
-    WITHDRAW,
-    TRANSFER
-  }
+enum TxType {
+  SUPPLY,
+  WITHDRAW,
+  TRANSFER
+}
 
-  struct ExtData {
-    address recipient;
-    uint256 scaledAmount;
-    address relayer;
-    uint256 scaledFee;
-    bytes encryptedOutput1;
-    bytes encryptedOutput2;
-  }
+struct ExtData {
+  address recipient;
+  uint256 scaledAmount;
+  address relayer;
+  uint256 scaledFee;
+  bytes encryptedOutput1;
+  bytes encryptedOutput2;
+}
 
-  struct Proof {
-    uint256[2] a;
-    uint256[2][2] b;
-    uint256[2] c;
-  }
+struct Proof {
+  uint256[2] a;
+  uint256[2][2] b;
+  uint256[2] c;
+}
 
-  struct ProofArgs {
-    Proof proof;
-    bytes32 root;
-    bytes32[] inputNullifiers;
-    bytes32[2] outputCommitments;
-    uint256 publicScaledAmount;
-    bytes32 extDataHash;
-  }
+struct ProofArgs {
+  Proof proof;
+  bytes32 root;
+  bytes32[] inputNullifiers;
+  bytes32[2] outputCommitments;
+  uint256 publicScaledAmount;
+  bytes32 extDataHash;
+}
 
-  struct AaveReserveConfigurationMap {
-    uint256 data;
-  }
+struct AaveReserveConfigurationMap {
+  uint256 data;
+}
 
-  struct AaveReserveData {
-    AaveReserveConfigurationMap configuration;
-    uint128 liquidityIndex;
-    uint128 currentLiquidityRate;
-    uint128 variableBorrowIndex;
-    uint128 currentVariableBorrowRate;
-    uint128 currentStableBorrowRate;
-    uint40 lastUpdateTimestamp;
-    uint16 id;
-    address aTokenAddress;
-    address stableDebtTokenAddress;
-    address variableDebtTokenAddress;
-    address interestRateStrategyAddress;
-    uint128 accruedToTreasury;
-    uint128 unbacked;
-    uint128 isolationModeTotalDebt;
-  }
+struct AaveReserveData {
+  AaveReserveConfigurationMap configuration;
+  uint128 liquidityIndex;
+  uint128 currentLiquidityRate;
+  uint128 variableBorrowIndex;
+  uint128 currentVariableBorrowRate;
+  uint128 currentStableBorrowRate;
+  uint40 lastUpdateTimestamp;
+  uint16 id;
+  address aTokenAddress;
+  address stableDebtTokenAddress;
+  address variableDebtTokenAddress;
+  address interestRateStrategyAddress;
+  uint128 accruedToTreasury;
+  uint128 unbacked;
+  uint128 isolationModeTotalDebt;
 }
