2c1161ac5a88872809b95b46c78ce2a65c71c71d
theNvN <thenvnsahu@gmail.com>
2023-01-22 04:32:58 +0530

    add constant

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -1,2 +1,10 @@
+import { BigNumber, utils } from 'ethers';
+
+const { keccak256, toUtf8Bytes } = utils;
+
 export const FIELD_SIZE =
   '21888242871839275222246405745257275088548364400416034343698204186575808495617';
+
+export const ZERO_LEAF = BigNumber.from(keccak256(toUtf8Bytes('privi')))
+  .mod(FIELD_SIZE)
+  .toHexString();
