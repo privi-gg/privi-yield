d2c8124ab157f7f3e58c5457f51f5bacbca5122b
theNvN <thenvnsahu@gmail.com>
2023-01-22 20:28:01 +0530

    fix: return types

--- apps/protocol/contracts/interfaces/IPool.sol
+++ apps/protocol/contracts/interfaces/IPool.sol
@@ -7,20 +7,20 @@ interface IPool {
     event CommitmentInserted(bytes32 commitment, uint256 leafIndex, bytes encryptedOutput);
     event NullifierUsed(bytes32 nullifier);
 
-    function supply(ProofArgs calldata args, ExtData calldata extData) external;
+    function supply(ProofArgs calldata args, ExtData calldata extData) external returns (uint256);
 
-    function withdraw(ProofArgs calldata args, ExtData calldata extData) external;
+    function withdraw(ProofArgs calldata args, ExtData calldata extData) external returns (uint256);
 
     function transfer(ProofArgs calldata args, ExtData calldata extData) external;
 
     function verifyProof(ProofArgs calldata args) external view returns (bool);
 
-    function getAavePoolAndReserveData() 
+    function getAavePoolAndReserveData()
         external
         view
         returns (
             address,
             AaveReserveData memory,
             uint256
-        )
+        );
 }
