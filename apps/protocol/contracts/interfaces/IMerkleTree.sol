da29eeaf10be483c7a317f5a2ea395276bdc46fb
theNvN <thenvnsahu@gmail.com>
2023-01-22 17:19:50 +0530

    refactor: add interface & calculate zero leaves

---
+++ apps/protocol/contracts/interfaces/IMerkleTree.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IMerkleTree {
+  function hashLeftRight(bytes32 left, bytes32 right) external view returns (bytes32);
+
+  function isKnownRoot(bytes32 root) external view returns (bool);
+
+  function getLastRoot() public view returns (bytes32);
+}
