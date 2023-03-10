4293fe5d26b692d845869361c53fb04eb6a3e93b
theNvN <thenvnsahu@gmail.com>
2023-03-11 00:52:39 +0530

    tests: protocol tests

--- apps/protocol/test/WTokenGateway.test.ts
+++ apps/protocol/test/WTokenGateway.test.ts
@@ -2,9 +2,13 @@ import { expect } from 'chai';
 import { artifacts, ethers } from 'hardhat';
 import { loadFixture, time } from '@nomicfoundation/hardhat-network-helpers';
 import { deployContract } from './helpers/utils';
-import { AAVE_POOL_ADDRESS_PROVIDER, ASSET_WETH, TREE_HEIGHT } from './helpers/constants';
+import {
+  AAVE_POOL_ADDRESS_PROVIDER,
+  ASSET_WETH as ASSET_WTOKEN,
+  TREE_HEIGHT,
+} from './helpers/constants';
 import { deployHasher } from './helpers/hasher';
-import { KeyPair, Utxo } from '@privi-yield/common';
+import { getScaledAmount, KeyPair, Utxo } from '@privi-yield/common';
 import { prepareSupplyProof, prepareWithdrawProof } from './helpers/proofs';
 import { parseEther, randomHex } from 'privi-utils';
 
@@ -18,7 +22,7 @@ const aTokenArtifact = artifacts.readArtifactSync('IAToken');
 const ONE_DAY = 24 * 60 * 60;
 const deltaAmount = parseEther('0.0001');
 
-describe('WTokenGateway', function () {
+describe.only('WTokenGateway', function () {
   async function fixture() {
     const aavePoolAddressProvider = await ethers.getContractAtFromArtifact(
       aavePoolAddressProviderArtifact,
@@ -26,39 +30,44 @@ describe('WTokenGateway', function () {
     );
     const aavePoolAddress = await aavePoolAddressProvider.getPool();
     const aavePool = await ethers.getContractAtFromArtifact(aavePoolArtifact, aavePoolAddress);
-    const reserve = await aavePool.getReserveData(ASSET_WETH);
+    const reserve = await aavePool.getReserveData(ASSET_WTOKEN);
 
-    const weth = await ethers.getContractAtFromArtifact(wTokenArtifact, ASSET_WETH);
-    const aWeth = await ethers.getContractAtFromArtifact(aTokenArtifact, reserve.aTokenAddress);
+    const wToken = await ethers.getContractAtFromArtifact(wTokenArtifact, ASSET_WTOKEN);
+    const aWToken = await ethers.getContractAtFromArtifact(aTokenArtifact, reserve.aTokenAddress);
 
     const hasher = await deployHasher();
     const verifier2 = await deployContract('contracts/verifiers/Verifier2.sol:Verifier');
     const verifier16 = await deployContract('contracts/verifiers/Verifier16.sol:Verifier');
-    const maxDepositAmt = utils.parseEther('10');
+    const sanctionsList = await deployContract('SanctionsListMock');
+    const maxSupplyAmt = parseEther('100');
 
-    const pool = await deployContract(
+    const poolImpl = await deployContract(
       'Pool',
       TREE_HEIGHT,
-      maxDepositAmt,
-      weth.address,
+      wToken.address,
       AAVE_POOL_ADDRESS_PROVIDER,
       hasher.address,
       verifier2.address,
-      verifier16.address
+      verifier16.address,
+      sanctionsList.address
     );
 
-    const wethGateway = await deployContract('WTokenGateway', weth.address);
+    const { data: initializeData } = await poolImpl.populateTransaction.initialize(maxSupplyAmt);
+    const poolProxy = await deployContract('PoolProxyMock', poolImpl.address, initializeData);
 
-    return { hasher, pool, weth, wethGateway, aWeth };
+    const pool = poolImpl.attach(poolProxy.address);
+
+    const wethGateway = await deployContract('WTokenGateway', wToken.address);
+
+    return { hasher, pool, wToken, wethGateway, aWToken };
   }
 
   it('supply works', async function () {
-    const { pool, wethGateway, weth, aWeth } = await loadFixture(fixture);
+    const { pool, wethGateway, wToken, aWToken } = await loadFixture(fixture);
 
     const keyPair = KeyPair.createRandom();
     const amount = utils.parseEther('1');
-    const scaledAmount = await pool.getAaveScaledAmount(amount);
-
+    const scaledAmount = await getScaledAmount(amount, pool);
     const supplyUtxo = new Utxo({ scaledAmount, keyPair });
 
     const { proofArgs, extData } = await prepareSupplyProof({
@@ -70,16 +79,16 @@ describe('WTokenGateway', function () {
       value: amount.add(deltaAmount),
     });
 
-    const poolBalance = await aWeth.balanceOf(pool.address);
+    const poolBalance = await aWToken.balanceOf(pool.address);
     expect(poolBalance).to.be.closeTo(amount, deltaAmount);
   });
 
-  it.only('withdraw works', async function () {
-    const { pool, wethGateway, weth, aWeth } = await loadFixture(fixture);
+  it('withdraw works', async function () {
+    const { pool, wethGateway, wToken, aWToken } = await loadFixture(fixture);
 
     const keyPair = KeyPair.createRandom();
     const supplyAmount = utils.parseEther('1');
-    const supplyScaledAmount = await pool.getAaveScaledAmount(supplyAmount);
+    const supplyScaledAmount = await getScaledAmount(supplyAmount, pool);
     const supplyUtxo = new Utxo({ scaledAmount: supplyScaledAmount, keyPair });
 
     const { proofArgs, extData } = await prepareSupplyProof({
@@ -92,7 +101,7 @@ describe('WTokenGateway', function () {
       value: supplyAmount.add(deltaAmount),
     });
 
-    const poolBalance = await aWeth.balanceOf(pool.address);
+    const poolBalance = await aWToken.balanceOf(pool.address);
     expect(poolBalance).to.be.closeTo(supplyAmount, deltaAmount);
 
     await time.increase(100 * ONE_DAY);
--- apps/protocol/test/helpers/aave.ts
+++
@@ -1,5 +0,0 @@
-import { BigNumber, BigNumberish, Contract } from 'ethers';
-
-export const getScaledAmount = (amount: BigNumberish, pool: Contract): BigNumber => {
-  return pool.getAaveScaledAmount(amount);
-};
