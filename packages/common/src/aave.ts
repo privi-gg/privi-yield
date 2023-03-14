19c5dd41b791aeeff0ec0f1e8b73057b93e6b747
theNvN <thenvnsahu@gmail.com>
2023-03-14 20:52:56 +0530

    refactor: return metadata too

--- packages/common/src/aave.ts
+++ packages/common/src/aave.ts
@@ -1,15 +1,21 @@
 import { BigNumber, BigNumberish, Contract } from 'ethers';
 import { rayDiv, rayMul } from '@aave/math-utils';
 
-export const getScaledAmount = async (amount: BigNumberish, pool: Contract) => {
+export const getAaveScaledAmountData = async (amount: BigNumberish, pool: Contract) => {
   const data = await pool.getAavePoolAndReserveData();
   const nextLiquidityIndex = BigNumber.from(data[2]).toString();
   const scaledAmount = rayDiv(BigNumber.from(amount).toString(), nextLiquidityIndex);
-  return BigNumber.from(scaledAmount.toString());
+  return {
+    scaledAmount: BigNumber.from(scaledAmount.toString()),
+    nextLiquidityIndex: BigNumber.from(nextLiquidityIndex),
+  };
 };
 
-export const getUserBalance = async (scaledBalance: BigNumberish, pool: Contract) => {
+export const getAaveUserBalanceData = async (scaledBalance: BigNumberish, pool: Contract) => {
   const normalizedIncome = await pool.getAaveReserveNormalizedIncome();
   const balance = rayMul(BigNumber.from(scaledBalance).toString(), normalizedIncome.toString());
-  return BigNumber.from(balance.toString());
+  return {
+    balance: BigNumber.from(balance.toString()),
+    normalizedIncome: BigNumber.from(normalizedIncome.toString()),
+  };
 };
